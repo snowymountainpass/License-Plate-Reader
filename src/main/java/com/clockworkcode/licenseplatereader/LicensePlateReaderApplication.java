76c8eb158e7e01a0df2a6531a004aef7cd0657a2
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2024-05-13 21:45:14 +0200

    Initial commit

---
+++ src/main/java/com/clockworkcode/licenseplatereader/LicensePlateReaderApplication.java
@@ -0,0 +1,13 @@
+package com.clockworkcode.licenseplatereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensePlateReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicensePlateReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clockworkcode/licenseplatereader/LicensePlateReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clockworkcode.licenseplatereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensePlateReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
